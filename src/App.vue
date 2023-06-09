ac219916bc5d44866b558cf3bcfa147bb72ef529
PatrickSampa <sampa.mendonca@gmail.com>
2023-06-09 19:22:23 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral/>
+    </div>
+    <div class="column is-three-quarter">
+      <FormularioVue/>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BarraLateral from './components/BarraLateral.vue';
+import FormularioVue from './components/FormularioVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    BarraLateral,
+    FormularioVue
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
---
+++ src/components/FormularioVue.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="box">
+        <div class="column">
+            <div class="column is-8" role="form" arial-label="formulario para criacao de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+            </div>
+            <div class="column">
+                <section>
+                    <strong>{{ tempoDecorrido }}</strong>
+                </section>
+                <button class="button" @click="iniciar">
+                    <span class="icon">
+                        <i class="fas fa-play"></i>
+                    </span>
+                    <span>play</span>
+                </button>
+                <button class="button" @click="finalizar">
+                    <span class="icon">
+                        <i class="fas fa-stop"></i>
+                    </span>
+                    <span>stop</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'FormularioVue',
+    //ESTADO INICIAL
+    data(){
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0,
+        }
+    },
+    computed: {
+        tempoDecorrido(){
+            return new Date(this.tempoEmSegundos * 1000 ).toISOString().substr(11,8)
+        }
+    },
+    methods: {
+        iniciar(){
+            this.cronometro = setInterval(() =>{
+                this.tempoEmSegundos +=1
+                console.log("cronometro: " + this.cronometro)
+            },1000)
+            console.log("INICIOU")
+        },
+        finalizar(){
+            clearInterval(this.cronometro)
+            console.log("tempoAposFinalizar: " + this.tempoEmSegundos);
+            console.log("FINALIZANDO")
+        }
+    }
+})
+
+
+</script>
